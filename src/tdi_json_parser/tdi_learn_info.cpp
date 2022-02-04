620a0c360c4b1bdb82bf14031519dab07ef089c4
Sayan Bandyopadhyay <sbandyopadhyay@barefootnetworks.com>
2022-02-04 00:06:09 -0800

    * Converting metadata funcs to be more C++ friendly (#12)
    * Converting metadata funcs to be more C++ friendly

--- src/tdi_json_parser/tdi_learn_info.cpp
+++ src/tdi_json_parser/tdi_learn_info.cpp
@@ -26,69 +26,23 @@
 
 namespace tdi {
 
-tdi_status_t LearnInfo::learnNameGet(std::string *name) const {
-  if (name == nullptr) {
-    LOG_ERROR("%s:%d %s ERROR Outparam passed null",
-              __func__,
-              __LINE__,
-              nameGet().c_str());
-    return TDI_INVALID_ARG;
-  }
-  *name = name_;
-  return TDI_SUCCESS;
-}
-
-tdi_status_t LearnInfo::learnIdGet(tdi_id_t *id) const {
-  if (id == nullptr) {
-    LOG_ERROR("%s:%d %s ERROR Outparam passed null",
-              __func__,
-              __LINE__,
-              nameGet().c_str());
-    return TDI_INVALID_ARG;
-  }
-  *id = id_;
-  return TDI_SUCCESS;
-}
-
-tdi_status_t LearnInfo::learnAnnotationsGet(AnnotationSet *annotations) const {
-  for (const auto &annotation : this->annotations_) {
-    (*annotations).insert(std::cref(annotation));
-  }
-  return TDI_SUCCESS;
-}
-tdi_status_t LearnInfo::dataFieldIdListGet(
-    std::vector<tdi_id_t> *id_vec) const {
-  if (id_vec == nullptr) {
-    LOG_ERROR("%s:%d %s Please pass mem allocated out param",
-              __func__,
-              __LINE__,
-              nameGet().c_str());
-    return TDI_INVALID_ARG;
-  }
+std::vector<tdi_id_t> LearnInfo::dataFieldIdListGet() const {
+  std::vector<tdi_id_t> id_vec;
   for (const auto &kv : learn_data_map_) {
-    id_vec->push_back(kv.first);
+    id_vec.push_back(kv.first);
   }
-  std::sort(id_vec->begin(), id_vec->end());
-  return TDI_SUCCESS;
+  std::sort(id_vec.begin(), id_vec.end());
+  return id_vec;
 }
 
-tdi_status_t LearnInfo::dataFieldIdGet(const std::string &name,
-                                       tdi_id_t *field_id) const {
-  if (field_id == nullptr) {
-    LOG_ERROR("%s:%d %s Please pass mem allocated out param",
-              __func__,
-              __LINE__,
-              nameGet().c_str());
-    return TDI_INVALID_ARG;
-  }
+tdi_id_t LearnInfo::dataFieldIdGet(const std::string &name) const {
   auto found = std::find_if(
       learn_data_map_.begin(),
       learn_data_map_.end(),
       [&name](const std::pair<const tdi_id_t, std::unique_ptr<DataFieldInfo>>
                   &map_item) { return (name == map_item.second->nameGet()); });
   if (found != learn_data_map_.end()) {
-    *field_id = (*found).second->idGet();
-    return TDI_SUCCESS;
+    return (*found).second->idGet();
   }
 
   LOG_ERROR("%s:%d %s Field \"%s\" not found in data field list",
@@ -96,28 +50,19 @@ tdi_status_t LearnInfo::dataFieldIdGet(const std::string &name,
             __LINE__,
             nameGet().c_str(),
             name.c_str());
-  return TDI_OBJECT_NOT_FOUND;
+  return 0;
 }
 
-tdi_status_t LearnInfo::dataFieldGet(
-    const tdi_id_t &field_id, const DataFieldInfo **data_field_info) const {
-  if (data_field_info == nullptr) {
-    LOG_ERROR("%s:%d %s Please pass mem allocated out param",
-              __func__,
-              __LINE__,
-              nameGet().c_str());
-    return TDI_INVALID_ARG;
-  }
+const DataFieldInfo *LearnInfo::dataFieldGet(const tdi_id_t &field_id) const {
   if (learn_data_map_.find(field_id) == learn_data_map_.end()) {
     LOG_ERROR("%s:%d %s Field \"%d\" not found in data field list",
               __func__,
               __LINE__,
               nameGet().c_str(),
               field_id);
-    return TDI_INVALID_ARG;
+    return nullptr;
   }
-  *data_field_info = learn_data_map_.at(field_id).get();
-  return TDI_SUCCESS;
+  return learn_data_map_.at(field_id).get();
 }
 
 }  // namespace tdi
