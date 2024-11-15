662886832c7e18debb8a416b4bd88cf39c16c87f
Bandyopadhyay, Sayan <sayan.bandyopadhyay@intel.com>
2024-11-15 23:18:43 +0000

    Adding code and files to be able to embed TDI shell with one command
    Signed-off-by: Bandyopadhyay, Sayan <sayan.bandyopadhyay@intel.com>

---
+++ cli/cli_example.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright(c) 2024 Intel Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "tdi_cli.h"
+
+int main() {
+  char loc[] = "/root";
+  char * p4_names[] = {
+        "Program 1",
+        "Program 2",
+  };
+
+  tdi_shell_start(loc,
+                  p4_names);
+  sleep(200);
+}
+
